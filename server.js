e4de6527bc8c8b83e2679d8ac7c77c12997c1de1
vivishno <vivishno@github.com>
2020-09-19 00:33:49 +0530

    ss

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 8092;
+var port = process.env.PORT || 8080;
 var fs = require('fs');
+const appInsights = require('applicationinsights');
+appInsights.setup();
+appInsights.start();
 
-var appInsights = require('applicationinsights');
-if(process.env.NODE_ENV == "production"){
-    appInsights.setup();
-    appInsights.start();
-}
-
-var server = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
     fs.readFile('index.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
         res.write(data);
         res.end();
     });
-});
-
-exports.listen = function () {
-    server.listen.apply(server, arguments);
-};
-  
-exports.close = function (callback) {
-    server.close(callback);
-};
-
-server.listen(port);
+}).listen(port);
